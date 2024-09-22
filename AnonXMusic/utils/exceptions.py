1a7ac2dcd7743e2b1195635f2685dc19ec0d6e51
cortox <urxoso450@gmail.com>
2024-09-22 14:14:06 +0530

    Update exceptions.py
    Signed-off-by: cortox <urxoso450@gmail.com>

--- AnonXMusic/utils/exceptions.py
+++ AnonXMusic/utils/exceptions.py
@@ -1,3 +1,7 @@
 class AssistantErr(Exception):
     def __init__(self, errr: str):
         super().__init__(errr)
+
+class DownloadError(Exception):
+    def __init__(self, errr: str):
+        super().__init__(errr)
